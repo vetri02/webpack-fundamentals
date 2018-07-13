24e0000aeca67cfe30426a802f8d0b73b4781cd0
Vetrichelvan Jeyapalpandy <vetri02@gmail.com>
2018-07-13 17:42:26 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import fooString from './foo.js';
+import bar from './bar';
+
+console.log(`Hello,${fooString} ${bar}`);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
